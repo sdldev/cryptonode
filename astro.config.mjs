48f56df8bd33bbd2a7e57227aeb5a5c19ca8d593
sdldev <sidolapakdev@gmail.com>
2024-03-19 21:19:15 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://cryptodemo.kangwie.my.id/",
+  integrations: [tailwind(), sitemap(), react(), mdx()]
+});
